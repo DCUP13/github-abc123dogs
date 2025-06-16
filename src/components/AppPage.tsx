a9d1258c856c8d63735b6870a069cac7f6b40eea
DCUP13 <144501544+DCUP13@users.noreply.github.com>
2025-06-15 22:13:53 -0400

    Updated AppPage.tsx

--- src/components/AppPage.tsx
+++ src/components/AppPage.tsx
@@ -32,7 +32,7 @@ interface AppPageProps {
 
 const cities = [
   'New York, NY', 'Los Angeles, CA', 'Chicago, IL', 'Houston, TX', 'Phoenix, AZ',
-  'Philadelphia, PA', 'San Antonio, TX', 'San Diego, 123CA', 'Dallas, TX', 'San Jose, CA'
+  'Philadelphia, PA', 'San Antonio, TX', 'San Diego, CA', 'Dallas, TX', 'San Jose, CA'
 ];
 
 const daysTillCloseOptions = [
