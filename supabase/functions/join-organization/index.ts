6879167860a5e79c6881df2123746db3f055ba42
DCUP13 <144501544+DCUP13@users.noreply.github.com>
2025-12-07 22:32:16 -0500

    Updated index.ts

--- supabase/functions/join-organization/index.ts
+++ supabase/functions/join-organization/index.ts
@@ -62,6 +62,7 @@ Deno.serve(async (req: Request) => {
 
         await supabase.auth.admin.updateUserById(userId, {
           password,
+          email_confirm: true,
           user_metadata: {
             needs_password_change: true
           }
